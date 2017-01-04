7af2102c61f7bf8a5d23988ebc266bea0fae09e8
Abhisek Swain <abhisekswain@Abhiseks-MacBook-Air.local>
2017-01-04 15:59:01 -0700

    Answer to Part III dictionaries

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,34 @@
-PLACE YOUR CODE HERE
+import csv
+import re
+from collections import defaultdict
+f = open("faculty.csv")
+csv_f = csv.reader(f)
+faculty_dict = {}
+professor_dict = {}
+firstline = True
+for row in csv_f:
+    if firstline:
+        firstline = False
+        continue
+    else:
+        name = row[0].split(" ")
+        last_name = name[-1]
+        details_list = [row[1], row[2], row[3]]
+        if last_name not in faculty_dict.keys():
+            faculty_dict[last_name] = [details_list]
+        else:
+            faculty_dict[last_name].append(details_list)
+
+        new_name = (name[0], name[-1])
+        if new_name not in professor_dict.keys():
+            professor_dict[new_name] = details_list
+#print faculty_dict
+#print "\n"
+#print professor_dict
+
+sorted_prof_dict = sorted(professor_dict.items(), key = lambda t: t[0][-1])
+print faculty_dict
+print "\n"
+print professor_dict
+print "\n"
+print sorted_prof_dict
