61e1ca3ee6187aa2373ab86104698360b294b732
Abhisek Swain <abhisekswain@Abhiseks-MacBook-Air.local>
2017-01-04 15:54:51 -0700

    Answer to Part II CSV

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,21 @@
-PLACE YOUR CODE HERE
+import csv
+import re
+f = open("faculty.csv")
+csv_f = csv.reader(f)
+email_address = []
+firstline = True
+for row in csv_f:
+    if firstline:
+        firstline = False
+        continue
+    else:
+        email_address.append(row[3])
+print email_address
+
+with open('emails.csv', 'wb') as f1:
+    writer = csv.writer(f1)
+    for val in email_address:
+        writer.writerow([val])
+
+f.close()
+f1.close()
